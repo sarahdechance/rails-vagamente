c43810155d1d117bd617544ee7d38a72e6a1f3ff
Samuel Zemmour <sazemmour@sergent-major.com>
2023-03-08 17:27:56 +0100

    query search bar

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,12 +2,17 @@ class PlacesController < ApplicationController
 
   def index
     # list my location & tag query's results
-    @places = Place.all
+    @places = current_user.places
     @markers = @places.geocoded.map do |place|
       {
         lat: place.latitude,
         lng: place.longitude
       }
+    end
+    if params[:query].present?
+      @query = params[:query]
+      sql_query = "address ILIKE :query"
+      @places = Place.where(sql_query, query: "%#{params[:query]}%")
     end
   end
 
