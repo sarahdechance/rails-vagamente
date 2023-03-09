177c25733a8b993f285e78721726730b7854864d
sarah dechance <sarah.dechance@gmail.com>
2023-03-09 14:30:34 +0100

    on continue les seeds, places et profiles autop

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   post 'profiles/define', to: 'profiles#define'
   # patch 'profiles/switch', to: 'profiles#switch'
 
-  get 'profiles/dashboard', to: 'pages#dashboard'
+  get 'user/dashboard', to: 'pages#dashboard'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_114126) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_115840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_114126) do
     t.float "latitude"
     t.float "longitude"
     t.text "description"
-    t.integer "type"
+    t.integer "genre"
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
