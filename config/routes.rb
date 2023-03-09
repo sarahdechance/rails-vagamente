79ac0bcd5cc20f3a18bc643478015d6f16b84a9f
Aubry NdawBastien <ndawaubry@gmail.com>
2023-03-09 14:38:33 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,16 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :places, only: %i[index show]
-  resources :bookmarks, except: %i[:new, :show] do
+  resources :bookmarks, except: %i[new show] do
     resources :bookmark_tags, only: %i[create]
   end
 
   resources :trips
   resources :bookmark_tags, only: %i[destroy]
 
-
   get 'profiles/definition', to: 'profiles#definition'
   post 'profiles/define', to: 'profiles#define'
   # patch 'profiles/switch', to: 'profiles#switch'
-
   get 'profiles/dashboard', to: 'pages#dashboard'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
