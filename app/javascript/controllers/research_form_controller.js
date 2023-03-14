8341bc01c3eca7145114b1d03fc83c77e69e3eec
sarah dechance <sarah.dechance@gmail.com>
2023-03-14 12:26:00 +0100

    commit avant pull

--- app/javascript/controllers/research_form_controller.js
+++ app/javascript/controllers/research_form_controller.js
@@ -2,8 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="research-form"
 export default class extends Controller {
+  static targets = ["element"]
+
   connect() {
     console.log("coucou")
+    // setTimeout => checker avec le code de la homepage controller
+  }
+
+  next() {
+
   }
 
 }
