2fd310d61ed237dcf4959f88e1fb16f0efb91097
sarah dechance <sarah.dechance@gmail.com>
2023-03-16 12:45:38 +0100

    1ere essai

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,31 +1,52 @@
 class PlacesController < ApplicationController
 
   def index
-    # list my location & tag query's results
 
     @places = Place.all
+
+    # ---- Afficher les résultats selon le lieu recherché ----
+    @query = params[:query]
+    if params[:query].present?
+      sql_query = "address ILIKE :query"
+      @places = @places.where(sql_query, query: "%#{params[:query]}%")
+    end
+
+
+    #  ---- Sélectionner les places selon le genre (resto, bar, club) recherché ----
+    @genre_enums = params[:genre]
+    @search_places = @places.select do |place|
+      @genre_enums.each do |enum|
+        place.genre == enum
+      end
+    end
+
+
+    # Je re-filtre par tags
+
+    # J'ordonne par le ranking du matching
+
+
+    # si résultat par catégory < 5 places => j'ajoute les best ranking hors tags
+
+
+
+
+    @places = @places.sample(3)
+
+    @best_matches = Place.all.sample(10)
+    @reco_bars = Place.where(genre: 1)
+    @reco_restaurants = Place.where(genre: 0)
+    @reco_club = Place.where(genre: 2)
+
+    @trips = current_user.trips # Pour la modale d'ajout des bookmarks => avoir la liste des trips déjà créés de mon user
+    @trip = Trip.new # Pour la modale d'ajout des bookmarks => créer une nouvelle instance de Trips
+
     # @markers = @places.geocoded.map do |place|
     #   {
       #     lat: place.latitude,
       #     lng: place.longitude
       #   }
-      # end
-      @query = params[:query]
-      if params[:query].present?
-        sql_query = "address ILIKE :query"
-        @places = @places.where(sql_query, query: "%#{params[:query]}%")
-      end
-      @search_places = @places.where()
-      @places = @places.sample(3)
-
-      @best_matches = Place.all.sample(10)
-      @reco_bars = Place.where(genre: 1)
-      @reco_restaurants = Place.where(genre: 0)
-      @reco_club = Place.where(genre: 2)
-
-
-      @trips = current_user.trips # Pour la modale d'ajout des bookmarks => avoir la liste des trips déjà créés de mon user
-      @trip = Trip.new # Pour la modale d'ajout des bookmarks => créer une nouvelle instance de Trips
+    # end
   end
 
   def show
